08d806edbe1fbbce3583d77cb2780c9d3a69ddef
vinferno <vinsonfernandez@gmail.com>
2022-10-19 21:54:26 -0700

    day9

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.burger');
-  const bar = document.querySelector('.bar');
-
-  burger.addEventListener('click', () =>{
-    bar.classList.toggle('closed');
-  })
